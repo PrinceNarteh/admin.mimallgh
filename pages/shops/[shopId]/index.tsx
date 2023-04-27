e3cc1036a8196b2e7b87c4bc378af93a9fdbac0e
Prince Narteh <prinarteh@gmail.com>
2023-04-27 12:34:14 +0000

    add shop detail page

--- pages/shops/[shopId]/index.tsx
+++ pages/shops/[shopId]/index.tsx
@@ -3,50 +3,32 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 
-import Back from "../../../components/Back";
-import { Button } from "../../../components/Button";
-import Card from "../../../components/Card";
-import Modal from "../../../components/Modal";
-import { api } from "../../../utils/api";
-import { capitalize } from "../../../utils/utilities";
-import Loader from "../../../components/Loader";
+import { Back, Button, Card, Modal, Loader } from "@/components";
+import { capitalize } from "@/utils/utilities";
+import { Shop } from "@/types";
+import { GetServerSideProps } from "next";
+import { getShop } from "@/services/shops";
 
-const ShopDetails = () => {
-  const {
-    query: { shopId },
-    push,
-  } = useRouter();
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { shopId } = context.query;
+  const data = await getShop(shopId as string);
+
+  return {
+    props: {
+      shop: data,
+    },
+  };
+};
+
+const ShopDetails = ({ shop }: { shop: Shop }) => {
   const [openDialog, setOpenDialog] = useState(false);
-
-  if (!shopId) {
-    push(`/shops`).catch((error) => console.log(error));
-  }
-  const { data, isLoading } = api.shops.getShopById.useQuery(
-    { shopId: shopId as string },
-    {
-      cacheTime: 10000,
-    }
-  );
-  const deleteShop = api.shops.deleteShop.useMutation();
+  const router = useRouter();
 
   const handleDelete = () => setOpenDialog(true);
 
-  if (shopId && isLoading) {
-    return <Loader />;
-  }
-
   function confirmDelete(choose: boolean) {
     if (choose) {
-      deleteShop.mutate(
-        { id: shopId as string },
-        {
-          onSuccess: () => {
-            toast.success("Admin deleted successfully!");
-            setOpenDialog(false);
-            push(`/shops`).catch((error) => console.log(error));
-          },
-        }
-      );
+      // delete shop
     } else {
       setOpenDialog(false);
     }
@@ -57,36 +39,30 @@ const ShopDetails = () => {
       <Back />
       <div className="mx-auto max-w-3xl space-y-5">
         <Card heading="Shop Details">
-          <div className="flex items-center justify-between py-4 px-4">
+          <div className="flex items-center justify-between py-4 px-4 bg-dark-gray">
             <div className="font-bold">Name</div>
-            <div>{data?.name}</div>
-          </div>
-          <div className="flex items-center justify-between bg-dark-gray py-4 px-4">
-            <div className="font-bold">Owner</div>
-            <div>{`${data?.owner.firstName || ""} ${
-              data?.owner.middleName || ""
-            } ${data?.owner.lastName || ""}`}</div>
+            <div>{shop?.name}</div>
           </div>
           <div className="flex items-center justify-between py-4 px-4">
             <div className="font-bold">Location</div>
-            <div>{capitalize(data?.location || "")}</div>
+            <div>{capitalize(shop?.location || "")}</div>
           </div>
           <div className="flex items-center justify-between bg-dark-gray py-4 px-4">
-            <div className="font-bold">Address</div>
-            <div>{data?.address || ""}</div>
+            <div className="font-bold">Map Direction</div>
+            <div>{shop?.mapDirection || ""}</div>
           </div>
           <div className="flex items-center justify-between py-4 px-4">
             <div className="font-bold">Phone Number</div>
-            <div>{data?.phoneNumber || ""}</div>
+            <div>{shop?.phoneNumber || ""}</div>
           </div>
           <div className="flex flex-col items-start bg-dark-gray py-4 px-4">
             <div className="mb-2 font-bold">Description</div>
-            <div>{data?.description || ""}</div>
+            <div>{shop?.description || ""}</div>
           </div>
         </Card>
 
-        {data?.branches.length
-          ? data.branches.map((branch, index) => (
+        {/* {shop?.branches.length
+          ? shop.branches.map((branch, index) => (
               <Card
                 heading={`${capitalize(branch.location) || ""} Branch`}
                 key={index}
@@ -105,10 +81,10 @@ const ShopDetails = () => {
                 </div>
               </Card>
             ))
-          : null}
+          : null} */}
 
         <div className="flex items-center justify-end gap-5">
-          <Link href={`/shops/${data?.id as string}/edit`} className="link">
+          <Link href={`/shops/${shop?.id as string}/edit`} className="link">
             Edit
           </Link>
           <Button onClick={handleDelete} variant="danger">
@@ -119,7 +95,7 @@ const ShopDetails = () => {
       {openDialog ? (
         <Modal
           onDialog={confirmDelete}
-          message={openDialog ? `${data?.name as string}` : ""}
+          message={openDialog ? `${shop?.name as string}` : ""}
         />
       ) : null}
     </div>
